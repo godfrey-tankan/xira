94cc3e5b327d7f7671703fd387144e30a2c66da8
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-30 17:40:43 +0200

    chore: Update BarChart keys and indexBy values
    This commit updates the keys and indexBy values in the BarChart component. The keys are changed to ["open", "closed", "pending", "expired", "solved"] and the indexBy is changed to "branch". This ensures that the BarChart accurately represents the data for different branches, improving the clarity and usability of the chart.

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -39,8 +39,8 @@ const BarChart = ({ isDashboard = false }) => {
           },
         },
       }}
-      keys={["hot dog", "burger", "sandwich", "kebab", "fries", "donut"]}
-      indexBy="country"
+      keys={["open", "closed", "pending", "expired", "solved"]}
+      indexBy="branch"
       margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
       padding={0.3}
       valueScale={{ type: "linear" }}
