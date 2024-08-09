dd973aaddfd431cccf4af0b42fccc3e40a9d33cb
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-09 14:41:26 +0200

    Refactor accept_ticket_response message in responses.py

--- backend/a_bot/responses.py
+++ backend/a_bot/responses.py
@@ -1,4 +1,4 @@
-accept_ticket_response = "*Hey{}*,\nThere is a ticket opened just now by {}\n\nTicket ID: {}\nDescription: {}\n\nPlease reply with this ticket id to assign it to yourself."
+accept_ticket_response = "*Hey{}*,\nThere is a ticket opened just now by {}\n\n*Ticket ID:* {}\n*Description:* {}\n\nPlease reply with this ticket id to assign it to yourself."
 greeting_messages = ["hello", "hi", "hey", "good day", "good morning", "good afternoon", "good evening"]
 thank_you_messages = ["thank you", "thanks", "thank you so much", "thanks a lot", "thank you very much"]
 ACCEPT_TICKET_MODE = "ticket_acceptance"
