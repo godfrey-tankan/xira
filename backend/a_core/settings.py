161598adeb7bc8207578e5db90def5eb27b8ae9c
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-09 10:05:13 +0200

    Remove unused SVG files

--- backend/a_core/settings.py
+++ backend/a_core/settings.py
@@ -147,13 +147,9 @@ MEDIA_URL = 'media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    # You can add more directories if needed
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
