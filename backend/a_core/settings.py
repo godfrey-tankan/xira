6829e7522dacdfe8828188009dc34faf0ac1bcb5
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-09 09:58:04 +0200

    chore: Update STATIC_ROOT in settings.py
    This commit updates the STATIC_ROOT in settings.py to 'staticfiles/' to align with the changes made in the code. This change improves the handling of static files in the application.

--- backend/a_core/settings.py
+++ backend/a_core/settings.py
@@ -148,7 +148,7 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
