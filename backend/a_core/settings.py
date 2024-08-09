002682ba738a7de9c4be535a77a1b5f6c28ec49f
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-09 09:52:55 +0200

    chore: Update STATIC_URL to 'staticfiles/' in settings.py

--- backend/a_core/settings.py
+++ backend/a_core/settings.py
@@ -147,7 +147,7 @@ MEDIA_URL = 'media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
+STATIC_URL = 'staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
